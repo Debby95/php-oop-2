3d983b89ea60d4f9dabc24383b867400e8892b07
Debby95 <debbyjacksonstar@hotmail.it>
2022-07-14 16:53:04 +0200

    + users

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 require_once "class/animal_food.php";
 require_once "class/animal_products.php";
 require_once "class/animal_toys.php";
+require_once "class/users.php";
 
 $products = new Products();
 $food = new Food();
 $toys = new Toys();
+$users = new Users();
 
 $products->setproductName("Nome Prodotto");
 $products->setproductPrice("Prezzo Prodotto");
@@ -18,4 +20,8 @@ $toys->setPalla("Rossa");
 $toys->setCorda("Intrecciata, gialla, blu e verde");
 $toys->setPeluche("Orsacchiotto marrone");
 
-var_dump($products, $food, $toys);
+$users->setFirstName("Martina");
+$users->setLastName("Verdi");
+$users->setEmail("martyverdi@email.it");
+
+var_dump($products, $food, $toys, $users);
